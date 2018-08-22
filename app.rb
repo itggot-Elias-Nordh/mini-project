a11bd6e98baaff1593d049b270bc807c6b2be650
kian00sh <kianoosh.geravesh@itggot.se>
2018-08-22 17:22:31 +0200

    View 1.2 (K)
    - css added
    - game1/timer fixed
    - game1.slim & game2.slim change
    
    * app.rb needs fix

--- app.rb
+++ app.rb
@@ -19,16 +19,25 @@ require_relative 'modules.rb'
     end
 
     get('/game1') do
-        className = session[:className]
-        difficulty = session[:difficulty].to_i
-        names = getList()[0][className].shuffle
-        if difficulty == 2
-        elsif difficulty == 1
-            size = (names.length/2).to_i
-            names = names[0...size]
-        else
-            size = (names.length/4).to_i
-            names = names[0...size]
-        end
-        slim(:game1, locals:{className: className, names: names})
+        # className = session[:className]
+        # difficulty = session[:difficulty].to_i
+        # names = getList()[0][className].shuffle
+        # if difficulty == 2
+        # elsif difficulty == 1
+        #     size = (names.length/2).to_i
+        #     names = names[0...size]
+        # else
+        #     size = (names.length/4).to_i
+        #     names = names[0...size]
+        # end
+        # slim(:game1, locals:{className: className, names: names})
+        slim :game1
+    end
+
+    post('/game2') do
+        redirect('/game2')
+    end
+    
+    get('/game2') do
+        slim(:game2)
     end
